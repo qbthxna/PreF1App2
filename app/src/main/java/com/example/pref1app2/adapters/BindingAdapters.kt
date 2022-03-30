26e196226cc06f48ece8754ea20ff856f7271a76
dogu1200 <dogukanyalcin_12@hotmail.com>
2022-03-30 13:59:17 +0300

    comment change BindingAdapters.kt

--- app/src/main/java/com/example/pref1app2/adapters/BindingAdapters.kt
+++ app/src/main/java/com/example/pref1app2/adapters/BindingAdapters.kt
@@ -6,9 +6,7 @@ import androidx.databinding.BindingAdapter
 import coil.load
 import com.example.pref1app2.R
 
-/**
- * Uses the Coil library to load an image by URL into an [ImageView]
- */
+
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
